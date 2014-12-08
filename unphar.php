a4f18b617d66d2ead163bae054dae8ada94576a5
PEMapModder <pemapmodder1970@gmail.com>
2014-12-08 22:10:54 +0800

    Added .phar filter for /unphar.php

--- unphar.php
+++ unphar.php
@@ -5,7 +5,7 @@
 <body><font face="Comic Sans MS">
 <h1>Unphar</h1>
 <form method="post" action="/unpharResult.php" enctype="multipart/form-data">
-	<p>Upload the phar file here:<br><input type="file" name="file"></p>
+	<p>Upload the phar file here:<br><input type="file" name="file" accept=".phar"></p>
 	<p><input type="submit" value="Convert"></p>
 </form>
 </font></body>
