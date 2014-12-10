c7a064b97e4e9f23fc7c78d841c29221e89a7be5
PEMapModder <pemapmodder1970@gmail.com>
2014-12-10 16:22:54 +0800

    Update pocketmine.php

--- pocketmine.php
+++ pocketmine.php
@@ -1,6 +1,6 @@
 <html><head><title>PocketMine Plugin Making Tools</title></head>
 <frameset rows="150,*">
-	<frame src="/pocketmine-frame-up.php#bottom" name="index" scrolling="on">
+	<frame src="/pocketmine-frame-up.php#title" name="index" scrolling="on">
 	<frame src="/phar.php" name="content">
 </frameset>
 </html>
