ac81665c849acc7d5d35df0fcc983d274837f6f5
PEMapModder <pemapmodder1970@gmail.com>
2014-12-13 17:59:50 +0800

    Updated debug.php for too large `lines` parameter

--- debug.php
+++ debug.php
@@ -10,6 +10,7 @@ if($ip !== "::1" and substr($ip, 0, 10) !== "192.168.1."){
 $lines =$_GET["lines"];
 $contents = explode("\r\n", file_get_contents("../logs/error.log"));
 $cnt = count($contents);
+$lines = min($cnt, $lines);
 for($i = 1; $i <= $lines; $i++){
 	echo $contents[$cnt - $i];
 	echo "<br>";
