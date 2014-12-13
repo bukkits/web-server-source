af5ce966faa7fd19674dd50768147862ec3bd2ae
PEMapModder <pemapmodder1970@gmail.com>
2014-12-13 17:19:07 +0800

    added debug.php

---
+++ debug.php
@@ -0,0 +1,17 @@
+<pre><?php
+
+$ip = _SERVER["REMOTE_ADDR"];
+if($ip !== "::1" and substr($ip, 0, 10) !== "192.168.1."){
+	http_response_code(403);
+	echo "403 Forbidden";
+}
+
+$lines =$_GET["lines"];
+$contents = explode("\r\n", file_get_contents("../logs/error.log"));
+$cnt = count($contents);
+for($i = 1; $i <= $lines; $i++){
+	echo $contents[$cnt - $i];
+	echo "<br>";
+}
+?>
+</pre>
